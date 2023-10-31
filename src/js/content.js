3c4c7e59232fa002bf4bb9ec8740d15f8fd0a683
Jannick Rask Lyng <jaxziiii174@gmail.com>
2023-10-31 17:14:37 +0100

    fixed a bug by changing nothing

--- src/js/content.js
+++ src/js/content.js
@@ -1,3 +1,4 @@
+console.log("fucking shit");
 const CLASS_LIST_XPATH = "/html/body/form/header/div[1]/nav[1]/ul/li[2]/div/div[4]/ul";
 const CLASS_SEARCH_XPATH = "/html/body/form/header/div[1]/nav[1]/ul/li[2]/div/div[2]/div[1]/input";
 let classListSearch = getElementByXpath(CLASS_SEARCH_XPATH);
